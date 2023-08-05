ab7316555d72fbcdd9d3cad5300aee00389e7193
WizVader <126302304+WizVader@users.noreply.github.com>
2023-08-05 17:14:03 +1200

    Created grid

---
+++ src/components/grid.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './grid.css';
+
+const Grid = () => {
+  return (
+    <div className="grid-container">
+      <div className="grid-item no-hover instruments">Instruments</div>
+      <div className="grid-item no-hover menu">Menu</div>
+      <div className="grid-item no-hover main">Main</div>
+      <div className="grid-item no-hover right">Right</div>
+      <div className="grid-item no-hover footer">Footer</div>
+      <div className="grid-item">Item 6</div>
+      <div className="grid-item">Item 7</div>
+      <div className="grid-item">Item 8</div>
+      <div className="grid-item">Item 9</div>
+      <div className="grid-item">Item 10</div>
+      <div className="grid-item">Item 11</div>
+      <div className="grid-item">Item 12</div>
+      <div className="grid-item">Item 13</div>
+      <div className="grid-item">Item 14</div>
+      <div className="grid-item">Item 15</div>
+      <div className="grid-item">Item 16</div>
+      <div className="grid-item">Item 17</div>
+      <div className="grid-item">Item 18</div>
+      <div className="grid-item">Item 19</div>
+      <div className="grid-item">Item 20</div>
+      <div className="grid-item">Item 21</div>
+      <div className="grid-item">Item 22</div>
+      <div className="grid-item">Item 23</div>
+      <div className="grid-item">Item 24</div>
+      <div className="grid-item">Item 25</div>
+    </div>
+  );
+}
+
+export default Grid;
