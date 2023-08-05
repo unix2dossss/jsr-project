9e2ee3fd97f84a6f386cab44749f983505b72c45
Luna <72428016+Lunas-Lab@users.noreply.github.com>
2023-08-06 11:13:47 +1200

    Working with placeholder for playing the sound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import {useState, useEffect} from 'react'
 import './App.css';
 import Pad from './components/pad';
 
+// Timer to generate event every second
+// Button to listen to time event
+//  If it's on, play a sound
+//  If it's off, do nothing
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function App() {
+
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {    sleep(1000).then(() => {
+    setTime((previousTime) => {return previousTime + 1})
+  })} , [time])
+
+  useEffect(() => {
+    // Doing something
+    // Check the button state
+    console.log(`Timing log`)
+    // 
+  }, [time])
   return (
     <div className="App">
-      <Pad />
+      <Pad time={time}/>
     </div>
   );
 }
--- src/components/pad.js
+++ src/components/pad.js
@@ -1,29 +1,26 @@
-import React, { Component, useRef } from "react";
+import React, { Component, useRef, useState } from "react";
 import './pad.css';
 import { useEffect } from "react";
+function Pad({time}) {
+  const [buttonState, setButtonState] = useState(false)
 
-class Pad extends React.Component {
-  handleTimerFired() {
-    alert("timer fired!");
-  }
+  useEffect(() => {
+    if(buttonState) {
+      // This is on state and trigger every time time change
+      console.log(`Play a sound when button is on at: ${time}`)
+      // 
+    }
 
-
-
-  constructor (){
-    super();
-    element = Pad.current;
-    element.addEventListener("timerFired", alert);
-  }
-
-  render(){
+  }, [time, buttonState])
   return (
     <div>
       <div>
-        <button className={'btn button-gradient button-one'}/>
+        <button className={'btn button-gradient button-one'} onClick={()=> {
+          setButtonState((previousState) => {return !previousState})
+        }}/>
       </div>
     </div>
   );
-  }
-};
+}
 
 export default Pad;
